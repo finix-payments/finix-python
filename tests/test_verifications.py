ca49c144056836acf3b02b2072579b566e9d1a7c
haoyang-finix <106175390+haoyang-finix@users.noreply.github.com>
2022-07-14 09:40:29 -0700

    split verification tests

--- tests/test_verifications.py
+++ tests/test_verifications.py
@@ -15,10 +15,33 @@ def config():
     return configuration
 
 
-def test_create_and_get(config):
+@pytest.fixture
+def c_verif(config):
     tmp_client = finix.FinixClient(config)
     req = CreateVerificationRequest(
         merchant='MUgWbPVvtKbzjKNNGKqdQYV7',
+        processor='DUMMY_V1',
+        tags=Tags(
+            test_key_100 = "test_val_100"
+        )
+    )
+    response = tmp_client.verifications.create(create_verification_request=req)
+    return response
+
+
+def test_get(config, c_verif):
+    tmp_client = finix.FinixClient(config)
+    id = c_verif.id
+    response = tmp_client.verifications.get(id)
+    assert response.id[:2] == 'VI'
+    assert response.processor == 'DUMMY_V1'
+    assert response.tags['test_key_100'] == 'test_val_100'
+
+
+def test_create(config):
+    tmp_client = finix.FinixClient(config)
+    req = CreateVerificationRequest(
+        merchant='MUucec6fHeaWo3VHYoSkUySM',
         processor='DUMMY_V1',
         tags=Tags(
             test_key_110 = "test_val_110"
@@ -28,7 +51,3 @@ def test_create_and_get(config):
     assert response.id[:2] == 'VI'
     assert response.processor == 'DUMMY_V1'
     assert response.tags['test_key_110'] == 'test_val_110'
-    response01 = tmp_client.verifications.get(response.id)
-    assert response01.id[:2] == 'VI'
-    assert response01.processor == 'DUMMY_V1'
-    assert response01.tags['test_key_110'] == 'test_val_110'
